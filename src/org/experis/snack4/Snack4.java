7d1500a54a1c59b82528942f179494b35d3d3252
MrArcano <salvatore-dario@hotmail.it>
2024-03-28 14:54:31 +0100

    [add]: snack 4

---
+++ src/org/experis/snack4/Snack4.java
@@ -0,0 +1,26 @@
+package org.experis.snack4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Snack4 {
+    public static void main(String[] args) {
+        // Snack4
+        // Data in input una stringa verificare se è palindroma
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Inserisci una parola palindroma: ");
+        char[] charWord = scan.nextLine().toLowerCase().toCharArray();
+
+        boolean isPali = true;
+
+        for (int i = 0; i < charWord.length; i++) {
+            if (charWord[i] != charWord[charWord.length - 1 - i]) {
+                isPali = false;
+                break;
+            }
+        }
+
+        System.out.println(isPali ? "Parola palindroma" : "Parola NON palindroma");
+    }
+}
